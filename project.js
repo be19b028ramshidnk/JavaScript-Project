996830eec85b5db094a7b5e820ce3a366a3f1e7d
RAMSHID N K <ramshidnk007@gmail.com>
2024-07-12 22:59:35 +0530

    final commit

--- project.js
+++ project.js
@@ -92,8 +92,9 @@ const spin = ()=>{
         }
 
     }
-    const reel = [[],[],[]]; // each arrays are column
+    const reel = []; // each arrays are column
     for ( let i =0; i< COLS; i ++){
+        reel.push([])
         const reelSymbol = [...symbols] // randomly select from symbols and continuesly remove  the selected symbol from symbols
         for (let j=0; j< ROWS; j++){
             const randomIndex = Math.floor (Math.random() * reelSymbol.length); // floor will helpto down to the lowest number
@@ -107,8 +108,78 @@ const spin = ()=>{
 
 }
 
-const reel = spin();
-console.log(reel);
-let balance = deposit(); // let means we can change the variables
-const numberLines = getnumberBets(); // const we cannot change the variable
-const bet = getBet(balance, numberLines)
+// next we have to transpose the 2D array
+const transpose = (reel) =>{
+    const rows =[];
+    for (let i =0; i <COLS; i++){
+        rows.push([]);
+        for(let j=0; j<ROWS;j++){
+            rows[i].push(reel[j][i]);
+        }
+    }
+    return rows
+}
+
+// print our slot machine
+const printRows =(rows)=>{
+    for ( const row of rows){
+        let rowString = "";
+        for ([i,symbol] of row.entries()){ // checking this is the last index or not
+            rowString +=symbol
+            if (i != row.length -1){
+                rowString +=" | "
+            }
+        } 
+        console.log(rowString)
+    }
+
+}
+
+// now we want to check the winning
+const getWinning = (rows,bet,Lines) =>{
+    let winnings =0;
+    for (let row =0; row < Lines; row++){
+        const symbols = rows[row];
+        let allSame =true;
+        for ( const symbol of symbols){
+            if (symbol !=symbols[0]){
+                allSame = false;
+                break; // exit from the for loop 
+            }
+        }
+        if (allSame){
+            winnings+= bet * SYMBOL_VALUES[symbols[0]]
+        }
+    }
+    return winnings
+}
+
+// we need to define function to reduce the bet amount from the balance
+const  game = () =>{
+    
+    let balance = deposit(); // let means we can change the variables
+
+    while (true){
+        console.log("Your Current Balance is" + balance.toString())
+        const numberLines = getnumberBets(); // const we cannot change the variable
+        const bet = getBet(balance, numberLines)
+        balance -= bet * numberLines;
+        const reel = spin();
+        const rows = transpose(reel);
+        
+        printRows(rows);
+        const winnings = getWinning(rows, bet, numberLines)
+        balance+=winnings;
+        console.log("You won RS " + winnings.toString())
+        if ( balance <=0){
+            console.log("You running out of money, you cannot proceed the game")
+            break;
+        }
+        const playAgain = prompt("You wanna play again (y/n) ?" )
+        if (playAgain != "y") break;
+    }   
+
+    
+}
+
+game()
